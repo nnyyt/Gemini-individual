e9bd1860e780b5e4d871e470ea0322f96d6f9a1b
nnyyt <109517395+nnyyt@users.noreply.github.com>
2025-02-20 16:37:02 +0700

    Update Main.java

--- HolidayTravelVehicles/Main.java
+++ HolidayTravelVehicles/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 	public static void main(String[] args) {
         Vehicle vehicle = Vehicle.createNewVehicle("123ABC", "Civic", "Hybrid", 2024, 35000.00);
-        System.out.println("Vehicle Created: " + vehicle.getName() + " - " + vehicle.getModel() + " (" + vehicle.getYear() + ")");
+        System.out.println("Vehicle Created: " + vehicle.getName() + " - " + vehicle.getModel() + " (" + vehicle.getYear() + ")\n"+ "Price: " +vehicle.getPrice()  
+        		
+        	);
     }
 }
